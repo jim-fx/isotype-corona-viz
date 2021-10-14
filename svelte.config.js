3afe48e0385f85ab5b0c1caa6e7b8753e57698e5
Max Richter <jim-x@web.de>
2021-10-14 13:10:22 +0200

    fix: correct base path for gh-pages

--- svelte.config.js
+++ svelte.config.js
@@ -2,6 +2,8 @@ import preprocess from 'svelte-preprocess';
 import WindiCSS from "vite-plugin-windicss"
 import adapterStatic from "@sveltejs/adapter-static";
 
+const dev = process.env.NODE_ENV === 'development';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
@@ -10,6 +12,9 @@ const config = {
 
 	kit: {
 		adapter: adapterStatic(),
+		paths: {
+			base: dev ? '' : '/isotype-corona-viz',
+		},
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
 		vite:{
